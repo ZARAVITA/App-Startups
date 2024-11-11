094a10d58e00563dfeb5e0b7ca414c75644b9374
ZARAVITA <zaravitamds18@gmail.com>
2024-11-11 14:55:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ if st.button("Prédire"):
 
     # Faire la prédiction
     prediction = regressor.predict(input_transformed)
-    st.write(f"Profit prédit : {round(prediction[0], 2)} Dhs")
+    st.write(f"Profit prédit : {round(prediction[0], 2)} Dhs par an")
 
 
 
